2715b0175375a7b878c8351d910c6081c71d0902
jimmyale3102 <jimmyale3102@gmail.com>
2024-07-27 10:30:04 +1000

    Initial Commit

---
+++ FavoritePlaces/FavoritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePlacesApp.swift
+//  FavoritePlaces
+//
+//  Created by Jimmy on 27/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
