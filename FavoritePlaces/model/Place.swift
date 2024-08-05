b5766c87890356cb824296edbbc408e57ea8740f
jimmyale3102 <jimmyale3102@gmail.com>
2024-08-05 14:42:06 +1000

    feat: Prepare Place struct to be persisted

--- FavoritePlaces/model/Place.swift
+++ FavoritePlaces/model/Place.swift
@@ -8,9 +8,40 @@
 import Foundation
 import MapKit
 
-struct Place: Identifiable {
+struct Place: Identifiable, Codable {
     var id = UUID()
     var name: String
     var coordinates: CLLocationCoordinate2D
     var isFavorite: Bool
+    
+    enum CodingKeys: CodingKey {
+        case id, name, isFavorite, latitude, longitude
+    }
+    
+    init(id: UUID = UUID(), name: String, coordinates: CLLocationCoordinate2D, isFavorite: Bool) {
+        self.id = id
+        self.name = name
+        self.coordinates = coordinates
+        self.isFavorite = isFavorite
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        
+        id = try container.decode(UUID.self, forKey: .id)
+        coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        name = try container.decode(String.self, forKey: .name)
+        isFavorite = try container.decode(Bool.self, forKey: .isFavorite)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(coordinates.latitude, forKey: .latitude)
+        try container.encode(coordinates.longitude, forKey: .longitude)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(isFavorite, forKey: .isFavorite)
+    }
 }
